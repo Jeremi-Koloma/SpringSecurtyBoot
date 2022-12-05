6b23c26979bb943ba9a7bc24722b68485523975a
Jeremi Koloma <kolomajeremi60@gmail.com>
2022-12-04 22:25:10 -0800

    Ajout de mon propre login et logout page fonctionnel

--- src/main/java/com/GrafDigital/Jeremy/BootSecurity/Security/SecurityConfiguration.java
+++ src/main/java/com/GrafDigital/Jeremy/BootSecurity/Security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 // Cette classe va etendre de webSecurityAdapter
@@ -40,7 +41,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .antMatchers("/api/public/users").hasRole("ADMIN")
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll() // Tous le monde doit avoir accès à notre page login
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login"); // Deconnexion
     }
 
 
